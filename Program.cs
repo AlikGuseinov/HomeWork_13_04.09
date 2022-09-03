4df0cb6b2f20821a2053033a7db1a6a9632563b3
Alik <9774952689@bk.ru>
2022-09-04 01:01:19 +0300

    Completed13Task

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a>99)
+
+Console.WriteLine("Третья цифра в числе "+a.ToString()+" это "+a.ToString()[2]);
+
+else
+
+Console.WriteLine("Третей цифры нету");
+
